cf5f7d8da4f5e4299cbc004f19507ad34b0b0e39
Davi Braz <davibraz.profissional@gmail.com>
2025-08-26 23:12:19 -0300

    correção do .env

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-# app/main.py
 
+
+import os
 from app import create_app
 
 def main():
     app = create_app()
-    app.run(debug=True, host="0.0.0.0", port=5050)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
